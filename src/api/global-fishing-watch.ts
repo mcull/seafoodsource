97dad186e952fced52e7b2ce6c7322897748b396
mcull <marc.cull@gmail.com>
2024-04-26 13:49:48 -0700

    troubleshooting netlify

--- src/api/global-fishing-watch.ts
+++ src/api/global-fishing-watch.ts
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
                                     "Authorization": `Bearer ${process.env.GATSBY_GLOBAL_FISHING_WATCH_API_KEY}`
                                 },
         });
+        console.log(response);
         const data = await response.json();
         console.log(data);
         res.status(200).json({message: data});        
