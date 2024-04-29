962bcdd00bf8f6edcdd0b518545651f468175085
mcull <marc.cull@gmail.com>
2024-04-29 10:28:31 -0700

    mapping hailmary

--- src/pages/page-1.tsx
+++ src/pages/page-1.tsx
@@ -1,17 +1,21 @@
-// Gatsby supports TypeScript natively!
 import React, { useEffect, useState } from "react"
 import { PageProps, Link } from "gatsby"
+import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const FirstPage = (props: PageProps) => {  
+const FirstPage = (props: PageProps) => { 
+  const position = {lat: 61.2176, lng: -149.8997};
   return (
     <Layout>
-      <SEO title="Page one" />
-      <h1>Hi from the first page</h1>
-      <p>Welcome to page 1 ({props.path})</p>
-      <Link to="/">Go back to the homepage</Link>
+      <div style={{width:"100%", height:"100%"}}>
+        <APIProvider apiKey='AIzaSyB7zqm5DZe69b4yhNABx90krs5Ck4u2MYE'>
+        <Map defaultCenter={position} defaultZoom={10}>
+          <Marker position={position} />
+        </Map>
+      </APIProvider>
+      </div>
     </Layout>
   )
 }
