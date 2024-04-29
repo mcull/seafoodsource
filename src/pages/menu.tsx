079aaf95b5f90e157c7c2236f1a90509986062c5
mcull <marc.cull@gmail.com>
2024-04-29 12:58:22 -0700

    simple video capture in the browser

---
+++ src/pages/menu.tsx
@@ -0,0 +1,73 @@
+// Gatsby supports TypeScript natively!
+import React, {useState} from "react"
+import { PageProps, Link } from "gatsby"
+import Webcam from "react-webcam";
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Menu = (props: PageProps) => {
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: { exact: "environment" }
+  };
+
+  const spinner = () => {
+    return (
+      <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-indigo-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+    )
+  }
+
+  const WebcamCapture = () => {
+    const webcamRef = React.useRef(null);
+    const [photo, setPhoto] = useState<string | null>(null);
+    const capture = React.useCallback(
+      () => {
+        const imageSrc = (webcamRef.current as any)?.getScreenshot();
+        setPhoto(imageSrc);
+      },
+      [webcamRef]
+    );
+
+    React.useEffect(
+      () => {
+        const devices = navigator.mediaDevices.enumerateDevices().then((devices) => console.log(devices));
+
+      });
+
+    return (
+      <>
+      <div className="block sm:hidden">
+        <div> {photo == null ? <button className="my-2 rounded-full bg-indigo-50 p-3 text-indigo-900 font-bold" onClick={capture}>📷 Capture photo</button> : 
+                               <button className="my-2 p-2 rounded-full bg-indigo-50 text-indigo-900 font-bold" onClick={() => {setPhoto(null)}}>🚫 Clear photo</button>
+        }</div>
+        <div>{photo && (<><div className="flex">{spinner()}<span>Processing photo... (not really) </span></div><br/><img src={photo}/></>)}</div>
+        <div>{!photo && (
+        <Webcam
+          audio={false}
+          height={720}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width={1280}
+          videoConstraints={videoConstraints}
+        />
+        )}</div>
+      </div>
+      <div className="hidden sm:block">
+        Desktop (or landcape modeo) detected... can't use the camera so please upload a photo of a menu (or something)... 
+      </div>
+      </>
+    );
+  };
+  return(
+    <Layout>
+      <SEO title="Page two" />
+      {WebcamCapture()}
+    </Layout>
+  )}
+
+export default Menu
