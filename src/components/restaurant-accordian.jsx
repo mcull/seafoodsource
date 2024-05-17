6f974c53fa0017e23f6e2d80f75513e8ab0c9674
mcull <marc.cull@gmail.com>
2024-05-17 13:32:35 -0700

    more css experimenting

--- src/components/restaurant-accordian.jsx
+++ src/components/restaurant-accordian.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { Accordion } from "flowbite-react";
  
 const RestaurantAccordion = ({restaurants}) => {
+  //workaround because Netlify is losing the Tailwind styles somehow
+  const titleStyle = {
+    textAlign: "left",
+    fontSize: "1.5em"
+  }
+
   console.log(restaurants);
   return (
     <Accordion>
         {restaurants.map((restaurant, index) => (
           
           <Accordion.Panel>
-            <Accordion.Title className="text-sm" style={{backgroundColor: "orange"}}>{restaurant.name}</Accordion.Title>
+            <Accordion.Title className="text-sm" style={{titleStyle}}>{restaurant.name}</Accordion.Title>
             <Accordion.Content>
               <div>{restaurant.formatted_address}</div>
               <div><span className="font-bold">Open now?</span> {restaurant.opening_hours.open_now ? "yes" : "no"}</div>
