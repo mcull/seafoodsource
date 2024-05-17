372f3e525c36611b0502e195ae807b7c638b56e1
mcull <marc.cull@gmail.com>
2024-05-17 12:34:22 -0700

    stabs in the netlify dark...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
+import withMT from "@material-tailwind/react/utils/withMT";
  
-module.exports = withMT({
+export default withMT({
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/pages/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
-    "./src/components/*.{js,jsx,ts,tsx}",
+    "public/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "public/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
