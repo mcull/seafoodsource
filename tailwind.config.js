1814416090c40e1112928083e8aec32d277ea872
mcull <marc.cull@gmail.com>
2024-05-17 12:03:36 -0700

    netlify issues...

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ const withMT = require("@material-tailwind/react/utils/withMT");
  
 module.exports = withMT({
   content: [
-    "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/pages//*.{js,jsx,ts,tsx}",
+    "./src/components//*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
