2962b8b045ecb88de5312270fd04a560e7001809
mcull <marc.cull@gmail.com>
2024-05-19 10:07:12 -0700

    undoing prev hack

--- src/components/restaurant-accordian.jsx
+++ src/components/restaurant-accordian.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import { Accordion } from "flowbite-react";
  
 const RestaurantAccordion = ({restaurants}) => {
-  //workaround because Netlify is losing the Tailwind styles somehow
-  const titleStyle = {
-    textAlign: "left",
-    fontSize: "1.5em"
-  }
+
 
   console.log(restaurants);
   return (
     <Accordion className="mt-6">
         {restaurants.map((restaurant, index) => (
           <Accordion.Panel>
-            <Accordion.Title className="text-sm" style={{titleStyle}}>{restaurant.name}</Accordion.Title>
+            <Accordion.Title>{restaurant.name}</Accordion.Title>
             <Accordion.Content>
               <div>{restaurant.formatted_address}</div>
               <div><span className="font-bold">Open now?</span> {restaurant?.opening_hours?.open_now ? "yes" : "no"}</div>
